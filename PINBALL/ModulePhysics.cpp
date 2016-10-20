28c9daf7e2227080ad4a30d38fc4e8527838ee0c
nicobabot <nico.babotaltisen@gmail.com>
2016-10-20 23:14:05 +0200

    circles fixed

--- PINBALL/ModulePhysics.cpp
+++ PINBALL/ModulePhysics.cpp
@@ -305,7 +305,7 @@ update_status ModulePhysics::PostUpdate()
 			{
 				b2CircleShape* shape = (b2CircleShape*)f->GetShape();
 				b2Vec2 pos = f->GetBody()->GetPosition();
-				App->renderer->DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), METERS_TO_PIXELS(shape->m_radius), 100, 100, 255);
+				App->renderer->DrawCircle(METERS_TO_PIXELS(pos.x)*SCREEN_SIZE, METERS_TO_PIXELS(pos.y)*SCREEN_SIZE, METERS_TO_PIXELS(shape->m_radius), 100, 100, 255);
 			}
 			break;
 
